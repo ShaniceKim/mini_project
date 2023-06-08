5ed2ad978fd3fb0a99cc44a6965a4de71ea7d6dd
ShaniceKim <qxzo1@naver.com>
2023-06-08 16:09:02 +0900

    Git Init, third Commit

--- myapp/app-10/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app-10/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -1,20 +1,20 @@
 package bitcamp.myapp.handler;
 
 import bitcamp.util.Prompt;
+import bitcamp.myapp.vo.Member;
 
 public class MemberHandler {
 
   static final int MAX_SIZE = 100;
-  static int[] no = new int[MAX_SIZE];
-  static String[] name = new String[MAX_SIZE];
-  static String[] email = new String[MAX_SIZE];
-  static String[] password = new String[MAX_SIZE];
-  static char[] gender = new char[MAX_SIZE];
+  static Member[] members = new Member[MAX_SIZE];
   static int userId = 1;
   static int length = 0;
 
   static final char MALE = 'M';
   static final char FEMALE = 'W';
+  
+  static final char EXIST = '1';
+  static final char NON_EXIST = '2';
 
   public static void inputMember() {
     if (!available()) {
@@ -22,35 +22,42 @@ public class MemberHandler {
       return;
     }
 
-    name[length] = Prompt.inputString("이름? ");
-    System.out.println(name[length]);
-    email[length] = Prompt.inputString("이메일? ");
-    password[length] = Prompt.inputString("암호? ");
-    gender[length] = inputGender((char)0);
+    Member m = new Member();
+    m.name = Prompt.inputString("이름? ");
+    m.email = Prompt.inputString("이메일? ");
+    m.password = Prompt.inputString("암호? ");
+    m.gender = inputGender((char)0);
+    m.history = inputHistory((char)0);
+    m.no = userId++;
 
-    no[length] = userId++;
-    length++;
+    // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게 
+    // 레퍼런스 배열에 담는다.
+    members[length++] = m;
   }
 
   public static void printMembers() {
     System.out.println("---------------------------------------");
-    System.out.println("번호, 이름, 이메일, 성별");
+    System.out.println("번호, 이름, 이메일, 성별, 방문이력");
     System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
+      Member m = members[i];
       System.out.printf("%d, %s, %s, %s\n", 
-        no[i], name[i], email[i], 
-        toGenderString(gender[i]));
+        m.no, m.name, m.email, 
+        toGenderString(m.gender),
+        toHistoryString(m.history));
     }
   }
 
   public static void viewMember() {
     String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
-      if (no[i] == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", name[i]);
-        System.out.printf("이메일: %s\n", email[i]);
-        System.out.printf("성별: %s\n", toGenderString(gender[i]));
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", m.name);
+        System.out.printf("이메일: %s\n", m.email);
+        System.out.printf("성별: %s\n", toGenderString(m.gender));
+        System.out.printf("방문이력: %s\n", toHistoryString(m.history));
         return;
       }
     }
@@ -61,18 +68,30 @@ public class MemberHandler {
     return gender == 'M' ? "남성" : "여성";
   }
 
+  private static String toHistoryString(char history) {
+    if (history == EXIST) {
+      return "있다(재 방문)";
+    } else if (history == NON_EXIST) {
+      return "없다(첫 방문)";
+    } else {
+      return "알 수 없다!";
+    }
+  }
+
   public static void updateMember() {
     String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
-      if (no[i] == Integer.parseInt(memberNo)) {
-        System.out.printf("이름(%s)? ", name[i]);
-        name[i] = Prompt.inputString("");
-        System.out.printf("이메일(%s)? ", email[i]);
-        email[i] = Prompt.inputString("");
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름(%s)? ", m.name);
+        m.name = Prompt.inputString("");
+        System.out.printf("이메일(%s)? ", m.email);
+        m.email = Prompt.inputString("");
         System.out.printf("새암호? ");
-        password[i] = Prompt.inputString("");
-        gender[i] = inputGender(gender[i]);
+        m.password = Prompt.inputString("");
+        m.gender = inputGender(m.gender);
         return;
+
       }
     }
     System.out.println("해당 번호의 회원이 없습니다!");
@@ -102,6 +121,30 @@ public class MemberHandler {
     }
   }
 
+  private static char inputHistory(char history) {
+    String label;
+    if (history == 0) {
+      label = "방문 이력?\n";
+    } else {
+      label = String.format("방문 이력(%s)?\n", toHistoryString(history));
+    }
+    loop: while (true) {
+      String menuNo1 = Prompt.inputString(label + 
+      "  1. 있다(재 방문)\n" + 
+      "  2. 없다(첫 방문)\n" + 
+      "> ");
+  
+      switch (menuNo1) {
+        case "1":
+          return EXIST;
+        case "2":
+          return NON_EXIST;
+        default:
+          System.out.println("무효한 번호입니다.");
+      }
+    }
+  }
+
   public static void deleteMember() {
     int memberNo = Prompt.inputInt("번호? ");
 
@@ -112,25 +155,16 @@ public class MemberHandler {
     }
 
     for (int i = deletedIndex; i < length - 1; i++) {
-      no[i] = no[i + 1];
-      name[i] = name[i + 1];
-      email[i] = email[i + 1];
-      password[i] = password[i + 1];
-      gender[i] = gender[i + 1];
+      members[i] = members[i + 1];
     }
 
-    no[length - 1] = 0;
-    name[length - 1] = null;
-    email[length - 1] = null;
-    password[length - 1] = null;
-    gender[length - 1] = (char) 0;
-
-    length--;
+    members[--length] = null;
   }
 
   private static int indexOf(int memberNo) {
     for (int i = 0; i < length; i++) {
-      if (no[i] == memberNo) {
+      Member m = members[i];
+      if (m.no == memberNo) {
         return i;
       }
     }
--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -12,6 +12,9 @@ public class MemberHandler {
 
   static final char MALE = 'M';
   static final char FEMALE = 'W';
+  
+  static final char EXIST = '1';
+  static final char NON_EXIST = '2';
 
   public static void inputMember() {
     if (!available()) {
@@ -24,6 +27,7 @@ public class MemberHandler {
     m.email = Prompt.inputString("이메일? ");
     m.password = Prompt.inputString("암호? ");
     m.gender = inputGender((char)0);
+    m.history = inputHistory((char)0);
     m.no = userId++;
 
     // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게 
@@ -33,14 +37,15 @@ public class MemberHandler {
 
   public static void printMembers() {
     System.out.println("---------------------------------------");
-    System.out.println("번호, 이름, 이메일, 성별");
+    System.out.println("번호, 이름, 이메일, 성별, 방문이력");
     System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
       Member m = members[i];
       System.out.printf("%d, %s, %s, %s\n", 
         m.no, m.name, m.email, 
-        toGenderString(m.gender));
+        toGenderString(m.gender),
+        toHistoryString(m.history));
     }
   }
 
@@ -52,6 +57,7 @@ public class MemberHandler {
         System.out.printf("이름: %s\n", m.name);
         System.out.printf("이메일: %s\n", m.email);
         System.out.printf("성별: %s\n", toGenderString(m.gender));
+        System.out.printf("방문이력: %s\n", toHistoryString(m.history));
         return;
       }
     }
@@ -60,6 +66,16 @@ public class MemberHandler {
 
   public static String toGenderString(char gender) {
     return gender == 'M' ? "남성" : "여성";
+  }
+
+  private static String toHistoryString(char history) {
+    if (history == EXIST) {
+      return "있다(재 방문)";
+    } else if (history == NON_EXIST) {
+      return "없다(첫 방문)";
+    } else {
+      return "알 수 없다!";
+    }
   }
 
   public static void updateMember() {
@@ -75,6 +91,7 @@ public class MemberHandler {
         m.password = Prompt.inputString("");
         m.gender = inputGender(m.gender);
         return;
+
       }
     }
     System.out.println("해당 번호의 회원이 없습니다!");
@@ -98,6 +115,30 @@ public class MemberHandler {
           return MALE;
         case "2":
           return FEMALE;
+        default:
+          System.out.println("무효한 번호입니다.");
+      }
+    }
+  }
+
+  private static char inputHistory(char history) {
+    String label;
+    if (history == 0) {
+      label = "방문 이력?\n";
+    } else {
+      label = String.format("방문 이력(%s)?\n", toHistoryString(history));
+    }
+    loop: while (true) {
+      String menuNo1 = Prompt.inputString(label + 
+      "  1. 있다(재 방문)\n" + 
+      "  2. 없다(첫 방문)\n" + 
+      "> ");
+  
+      switch (menuNo1) {
+        case "1":
+          return EXIST;
+        case "2":
+          return NON_EXIST;
         default:
           System.out.println("무효한 번호입니다.");
       }
